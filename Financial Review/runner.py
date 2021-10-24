82c88ce95f128766ccbec1e5643b1d6779827559
5top1t <jarryngandjui@gmail.com>
2021-10-24 05:26:50 -0400

    update arguments

--- Financial Review/runner.py
+++ Financial Review/runner.py
@@ -12,7 +12,7 @@ parser = argparse.ArgumentParser(description="Financial runner argument parser")
 parser.add_argument(
     '-bank', choices=[BOA_PROCESSOR_ARG],help='Transaction processor')
 parser.add_argument(
-    '-ifp', help='Input file path')
+    '-fp', help='Transaction file path')
 
 args = parser.parse_args()
 if args.bank == BOA_PROCESSOR_ARG:
