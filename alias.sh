0e7848eef496e236a1acb73fde7c8050e8c2fa6c
5top1t <jarryngandjui@gmail.com>
2021-10-24 05:25:07 -0400

    alias file

--- alias.sh
+++ alias.sh
@@ -1,3 +1,20 @@
+# Alias edit
+alias alias-edit='vi ~/.aliases'
+
+# Aws Credentials
+alias aws-credentials='vi ~/.aws/credentials'
+
+# Application
+# todo.txt-cli (https://github.com/todotxt/todo.txt-cli)
+alias t=todo.sh
+
+# Servers
+# BeanTown Café
+alias jarry@btc_react='ssh -i ~/Moi/Git/.pem/caffe-library.pem jarry@100.25.117.241'
+alias jarry@btc_node='ssh -i ~/Moi/Git/.pem/caffe-library.pem jarry@54.90.37.246'
+alias ubuntu@btc_react='ssh -i ~/Moi/Git/.pem/caffe-library.pem ubuntu@100.25.117.241'
+alias ubuntu@btc_node='ssh -i ~/Moi/Git/.pem/caffe-library.pem ubuntu@54.90.37.246'
+
 # Git Commands
 # -----------------------------
 # git add -u : updates existing files that are tracked and does not add new ones
