a653252904ce859d6c81cad48afb8a766bedff73
Jarry Ngandjui <jarryngandjui@gmail.com>
2023-07-02 23:44:44 -0400

    configure mac

---
+++ setup/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+PROJECTS=~/Projects
+NOTES=$PROJECTS/Notes/
+SETUP=$NOTES/setup
+
+APPS=$SETUP/install_apps.sh
+TERM=$SETUP/configure_term.shA
+
+
+# Make the scripts executable 
+chmod +x $APPS
+chmod +x $TERM
---
+++ setup/install_apps.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+echo "Installing Chrome..."
+if [ -x "$(command -v google-chrome-stable)" ]; then
+  echo "Chrome is already installed."
+else
+  brew cask install google-chrome
+  echo "Chrome has been installed successfully."
+fi
+
+echo "Installing Docker..."
+if [ -x "$(command -v docker)" ]; then
+  echo "Docker is already installed."
+else
+  brew install docker
+  echo "Docker has been installed successfully."
+fi
+
+
+echo "Installing VSCode..."
+if [ -x "$(command -v code)" ]; then
+  echo "VSCode is already installed."
+else
+  brew cask install visual-studio-code
+  echo "VSCode has been installed successfully."
+fi
+
+
+echo "Installing Notion..."
+if [ -x "$(command -v notion)" ]; then
+  echo "Notion is already installed."
+else
+  brew cask install notion
+  echo "Notion has been installed successfully."
+fi
+
