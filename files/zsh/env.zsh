93a7d751770cb36addea23d49f8e69edb23ba5f0
5top1t <jarryngandjui@gmail.com>
2024-02-04 09:33:58 -0500

    Load executables

--- files/zsh/env.zsh
+++ files/zsh/env.zsh
@@ -1,6 +1,19 @@
 #!/bin/bash
+# Executables
+export PATH="/opt/homebrew/bin:$PATH"
+export PATH="/opt/homebrew/sbin:$PATH"
+export PATH="/usr/local/bin:$PATH"
+export PATH="/System/Cryptexes/App/usr/bin:$PATH"
+export PATH="/usr/bin:$PATH"
+export PATH="/bin:$PATH"
+export PATH="/usr/sbin:$PATH"
+export PATH="/sbin:$PATH"
+export PATH="/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/local/bin:$PATH"
+export PATH="/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/bin:$PATH"
+export PATH="/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/appleinternal/bin:$PATH"
+export PATH="$HOME/Library/Application Support/JetBrains/Toolbox/scripts:$PATH"
 
-# Main diecotries 
+# Quick access files and directories
 export PROJECTS=$HOME/Projects
 export DOTFILES=$HOME/dotfiles
 export CONFIG=$DOTFILES/files
