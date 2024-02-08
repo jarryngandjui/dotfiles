dcda1002aced90c0d482c5c1fa42d2095fe04e20
Jarry Ngandjui <jarryngandjui@gmail.com>
2024-02-07 20:04:21 -0500

    Update nvim

--- files/nvim/init.lua
+++ files/nvim/init.lua
@@ -37,6 +37,9 @@ I hope you enjoy your Neovim journey,
 P.S. You can delete this when you're done too. It's your config now :)
 --]]
 
+-- Set the shell to Zsh
+vim.cmd('set shell=/opt/homebrew/bin/zsh')
+
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
@@ -261,6 +264,16 @@ require('lazy').setup({
     end
   },
 
+  -- Enable autocompletion
+  { "rafamadriz/friendly-snippets" },
+  {
+    "tpope/vim-surround",
+    config = function()
+      vim.cmd("runtime macros/surround.vim")
+    end,
+    event = "BufRead",
+  },
+
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.
@@ -433,7 +446,7 @@ vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = 
 vim.defer_fn(function()
   require('nvim-treesitter.configs').setup {
     -- Add languages to be installed here that you want installed for treesitter
-    ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash' },
+    ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash', 'html' },
 
     -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
     auto_install = false,
@@ -445,6 +458,13 @@ vim.defer_fn(function()
     modules = {},
     highlight = { enable = true },
     indent = { enable = true },
+    autotag = {
+      enable = true,
+      enable_rename = true,
+      enable_close = true,
+      enable_close_on_slash = true,
+      filetypes = { "html" , "xml" },
+    },
     incremental_selection = {
       enable = true,
       keymaps = {
