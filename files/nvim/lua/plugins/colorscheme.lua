44bbf64d7f7c9f0ff88140e352fb6bb03c68771c
5top1t <jarryngandjui@gmail.com>
2024-06-17 04:02:09 -0400

    colorscheme catppuccin

--- files/nvim/lua/plugins/colorscheme.lua
+++ files/nvim/lua/plugins/colorscheme.lua
@@ -1,13 +1,23 @@
 -- poimandres configuration
 return {
-  'olivercederborg/poimandres.nvim',
-  lazy = false,
+  -- 'olivercederborg/poimandres.nvim',
+  -- lazy = false,
+  -- priority = 1000,
+  -- config = function()
+  --   require('poimandres').setup {}
+  -- end,
+  -- init = function()
+  --   vim.cmd("colorscheme poimandres")
+  -- end
+  "catppuccin/nvim",
+  name = "catppuccin",
   priority = 1000,
   config = function()
-    require('poimandres').setup {}
+    require("catppuccin").setup({
+            integrations = {},
+    })
+    vim.cmd([[colorscheme catppuccin]])
   end,
-  init = function()
-    vim.cmd("colorscheme poimandres")
-  end
+
 }
 
