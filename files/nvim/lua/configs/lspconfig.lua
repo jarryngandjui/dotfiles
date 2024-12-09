68d9b636b95557e9eb6d66b0988ca5f1628584e7
5top1t <jarryngandjui@gmail.com>
2024-12-09 05:33:00 -0600

    update: nvcad lsp

--- files/nvim/lua/configs/lspconfig.lua
+++ files/nvim/lua/configs/lspconfig.lua
@@ -4,7 +4,15 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls" }
+local servers = { 
+  "html",
+  "cssls",
+  "lua_ls",
+  "pylsp",
+  "ruff",
+  "taplo",
+  "tsserver",
+}
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
