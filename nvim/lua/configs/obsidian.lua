7f12f2f9958db112d6ac0510213377b9d066f8e4
5top1t <jarryngandjui@gmail.com>
2025-12-02 01:42:22 -0500

    enable obsidian outside vaults

--- nvim/lua/configs/obsidian.lua
+++ nvim/lua/configs/obsidian.lua
@@ -14,7 +14,24 @@ local options = {
   },
   attachments = {
     img_folder = "7.\\ Files",  -- default folder to save images
-  }
+  },
+  -- use obsidian outside of vault
+  {
+    name = "no-vault",
+    path = function()
+      -- alternatively use the CWD:
+      -- return assert(vim.fn.getcwd())
+      return assert(vim.fs.dirname(vim.api.nvim_buf_get_name(0)))
+    end,
+    overrides = {
+      notes_subdir = vim.NIL,  -- have to use 'vim.NIL' instead of 'nil'
+      new_notes_location = "current_dir",
+      templates = {
+        folder = vim.NIL,
+      },
+      disable_frontmatter = true,
+    },
+  },
 }
 
 return options
