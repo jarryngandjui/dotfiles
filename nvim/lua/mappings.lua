caba273d0155cdcc67f2804f951c52c6bd5c2f4a
5top1t <jarryngandjui@gmail.com>
2025-12-09 19:15:39 -0500

    harpoon mappings

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -37,15 +37,20 @@ local harpoon = require("harpoon")
 -- REQUIRED
 harpoon:setup()
 -- REQUIRED
-map("n", "<leader>a", function() harpoon:list():add() end, { desc = "Harpoon: Add current file to list" })
-map("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Harpoon: Toggle Quick Menu" })
+map("n", "<leader>hpa", function() harpoon:list():add() end, { desc = "Harpoon: Add current file to list" })
+map("n", "<leader>hpl", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Harpoon: Toggle Quick Menu" })
 
--- Direct Jumps (select first 4 files)
-map("n", "<C-h>", function() harpoon:list():select(1) end, { desc = "Harpoon: Go to item 1" })
-map("n", "<C-t>", function() harpoon:list():select(2) end, { desc = "Harpoon: Go to item 2" })
-map("n", "<C-n>", function() harpoon:list():select(3) end, { desc = "Harpoon: Go to item 3" })
-map("n", "<C-s>", function() harpoon:list():select(4) end, { desc = "Harpoon: Go to item 4" })
+-- Direct Jumps (select first 9 files)
+map("n", "<leader>hp1", function() harpoon:list():select(1) end, { desc = "Harpoon: Go to item 1" })
+map("n", "<leader>hp2", function() harpoon:list():select(2) end, { desc = "Harpoon: Go to item 2" })
+map("n", "<leader>hp3", function() harpoon:list():select(3) end, { desc = "Harpoon: Go to item 3" })
+map("n", "<leader>hp4", function() harpoon:list():select(4) end, { desc = "Harpoon: Go to item 4" })
+map("n", "<leader>hp5", function() harpoon:list():select(5) end, { desc = "Harpoon: Go to item 5" })
+map("n", "<leader>hp6", function() harpoon:list():select(6) end, { desc = "Harpoon: Go to item 6" })
+map("n", "<leader>hp7", function() harpoon:list():select(7) end, { desc = "Harpoon: Go to item 7" })
+map("n", "<leader>hp8", function() harpoon:list():select(8) end, { desc = "Harpoon: Go to item 8" })
+map("n", "<leader>hp9", function() harpoon:list():select(9) end, { desc = "Harpoon: Go to item 9" })
 
--- Toggle previous & next buffers stored within Harpoon list
-map("n", "<C-S-P>", function() harpoon:list():prev() end, { desc = "Harpoon: Previous file in list" })
-map("n", "<C-S-N>", function() harpoon:list():next() end, { desc = "Harpoon: Next file in list" })
+-- Toggle previous & next buffers
+map("n", "<leader>hpp", function() harpoon:list():prev() end, { desc = "Harpoon: Previous file in list" })
+map("n", "<leader>hpn", function() harpoon:list():next() end, { desc = "Harpoon: Next file in list" })
